ca37b7c5b512852cd39eef3f92d4856cdc306966
AnitaBL <anitalindmoen@gmail.com>
2023-03-26 20:08:30 +0200

    Prosjekter

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -7,23 +7,28 @@ const Projects = () => {
     <div id="projects" className="w-full h-screen bg-[#000000] text-white">
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full grid grid-cols-2 gap-8">
-        <img
+          <img
+              className="w-50"
+              src={klar_logo_invert}
+              alt="Sanctus Omegas Broderskab"
+            />
+            <img
               className="w-50"
               src={klar_logo_invert}
               alt="Sanctus Omegas Broderskab"
             />
           <div className="sm:text-left pb-8 pl-4">
             <p className="text-8xl justify-center font-lincoln font-bold inline border-b-4 border-gray-700">
-              Verv
+              Prosjekter
             </p>
             <p className="text-2xl justify-center">
               Frivillig og verv er ett fast innslag i min studie hverdag. Selv har jeg deltatt...
             </p>
-            <img
+            {/* <img
               className="w-10"
               src={vandrebar}
               alt="Kielder Vandrebar"
-            />
+            /> */}
           </div>
         </div>
       </div>
